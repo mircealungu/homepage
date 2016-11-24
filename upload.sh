a7af535892efecb692997ff978f7a4ca6a39dbe3
Mircea Lungu <mircea.lungu@gmail.com>
2016-11-24 02:06:40 +0100

    small changes

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ TARGET_FOLDER=../mircealungu.github.io
 
 rm -rf '$TARGET_FOLDER/*'
 hugo -d $TARGET_FOLDER
-(cd $TARGET_FOLDER; git add *; git commit -a; git push)
+(cd $TARGET_FOLDER; git add *; git commit -a; git push -f)
